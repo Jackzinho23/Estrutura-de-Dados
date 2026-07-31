9c324dc6423c197816000c0066e2b7500a5bbbf9
Jackzinho23 <iranferreira944@gmail.com>
2026-07-30 22:23:34 -0300

    tabuleiro usando array atualização

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,3 +1,10 @@
-x = True/False
-if not x
-return print ("sucesso")
+tabuleiro = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["X", "O", "X"]
+]
+print(tabuleiro[0][0])
+print(tabuleiro[1][2])
+
+# alterar uma posição
+tabuleiro[0][1] = "X"
